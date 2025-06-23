75e71ed62883d807e05297ff01e8daa3f73043a3
poper12 <samyakdhengle89@gmail.com>
2025-06-23 12:16:12 +0530

    config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ from logging.handlers import RotatingFileHandler
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "20445873"))
-API_HASH = os.environ.get("API_HASH", "057fd0be9d7c38526b143c582bceb24b")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
 
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5543390445"))
